e78ee82aafd0b65bb35d3f435c0649e149ae37ba
Daniel Rodrigues <danielprodrigues0@gmail.com>
2025-01-11 12:21:27 -0300

    refactor merge function: rename 'element' to 'val' for consistency in linked list sorting

--- sorting/mergesort/linked-list.js
+++ sorting/mergesort/linked-list.js
@@ -17,7 +17,7 @@ function merge(l1, l2) {
   let tail = head;
 
   while (l1 && l2) {
-    if (l1.element < l2.element) {
+    if (l1.val < l2.val) {
       tail.next = l1;
       l1 = l1.next;
     } else {
